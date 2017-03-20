8434b5463665d6daac22d57d82bed25325bd1c64
jeanette montesano <escapetheearth@gmail.com>
2017-03-20 14:27:26 -0400

    clock

---
+++ Desktop/clock/clock.js
@@ -0,0 +1,27 @@
+alert("hello kitty")
+
+//lets display the current time
+var d, h, m, s, color;
+function displayTime() {
+	d = new Date(); //new data object
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
+	
+	
+	if(h <= 9) h = '0'+h;
+	if(m <= 9) m = '0'+m;
+	if(s <= 9) s = '0'+s;
+	
+	color = "#"+h+m+s;
+	
+	document.body.style.background = color;
+	
+	document.getElementById("hex").innerHTML = color;
+	
+	
+	setTimeout(displayTime, 1000);
+}
+
+
+displayTime();
